cb3f3b40a72b83369a15c7943ac7b27b7d6521ce
sulaimanbehzad <sulaimanbehzad@gmail.com>
2021-08-21 11:51:36 +0430

    servolinks axis problem solved YAY

---
+++ Assets/GetGlobalLocation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+	{
+		
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+/*
+		Vector3[] position_array = new Vector3[1000];
+        Vector3 LocalPosition = gameObject.GetComponent<Transform>().position;
+		Vector3 GloabalPosition = gameObject.transform.position;
+		position_array.add(GlobalPosition);
+		System.IO.File.WriteAllLines("locations.txt", position_array.ToString()):
+*/
+    }
+}
