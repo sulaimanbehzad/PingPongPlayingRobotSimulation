bd15288b46eb22aa20b2a19d75bb2a3865d1423f
sulaimanbehzad <sulaimanbehzad@gmail.com>
2021-08-27 11:40:54 +0430

    script for reading trajectory csv file is able to read and allocate in two lists angle and time

--- Assets/Scripts/MoveMotor.cs
+++ Assets/Scripts/MoveMotor.cs
@@ -7,23 +7,27 @@ using System.IO;
 public class MoveMotor : MonoBehaviour
 {
     // Start is called before the first frame update
+    private List<string> timeList = new List<string>();
+    private List<string> angleList = new List<string>();
     void Start()
     {
         using(var reader = new StreamReader(@"C:\Users\Sulaiman's PC\PingPongPlayingRobotSimulation\Assets\Scripts\Trajectory_LinCirLin_MaxAcc.csv_Teta_BSpline_Motor1.csv"))
         {
-            List<string> listA = new List<string>();
-            List<string> listB = new List<string>();
             while (!reader.EndOfStream)
             {
                 var line = reader.ReadLine();
-                var values = line.Split(';');
+                var values = line.Split(',');
 
-                listA.Add(values[0]);
-                listB.Add(values[1]);
+                timeList.Add(values[0]);
+                angleList.Add(values[1]);
+                Debug.Log("value 0");
+                Debug.Log(values[0].ToString());
+                Debug.Log("value 1");
+                Debug.Log(values[1].ToString());
             }
             reader.Close();
         }
-        Console.WriteLine("hello");
+        Debug.Log(timeList.Count.ToString());
     }
 
     // Update is called once per frame
