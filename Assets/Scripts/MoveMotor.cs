b71cd3e9150195a94f732e1f2972b9156c063ca8
sulaimanbehzad <sulaimanbehzad@gmail.com>
2021-08-28 19:34:40 +0430

    successfully read csv and servomotors follow trajectory

--- Assets/Scripts/MoveMotor.cs
+++ Assets/Scripts/MoveMotor.cs
@@ -6,12 +6,41 @@ using System.IO;
 
 public class MoveMotor : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // This two lists will hold the csv data
     private List<string> timeList = new List<string>();
     private List<string> angleList = new List<string>();
+
+    private int ang_cnt=0;
+    private float y_angle = 0f;
+    // Start is called before the first frame update
     void Start()
     {
-        using(var reader = new StreamReader(@"C:\Users\Sulaiman's PC\PingPongPlayingRobotSimulation\Assets\Scripts\Trajectory_LinCirLin_MaxAcc.csv_Teta_BSpline_Motor1.csv"))
+        // reader object takes path as input
+        // !!! path is system dependent
+        // TODO: make path system independent
+        String path_CSV1 = @"C:\Users\Sulaiman's PC\PingPongPlayingRobotSimulation\Assets\Scripts\Trajectory_LinCirLin_MaxAcc.csv_Teta_BSpline_Motor1.csv";
+        String path_CSV2 = @"C:\Users\Sulaiman's PC\PingPongPlayingRobotSimulation\Assets\Scripts\Trajectory_LinCirLin_MaxAcc.csv_Teta_BSpline_Motor2.csv";
+        String path_CSV3 = @"C:\Users\Sulaiman's PC\PingPongPlayingRobotSimulation\Assets\Scripts\Trajectory_LinCirLin_MaxAcc.csv_Teta_BSpline_Motor3.csv";
+        String cur_path = "";
+        String cur_g_obj = gameObject.name;
+
+        switch (cur_g_obj)
+        {
+            case "SL1":
+                cur_path = path_CSV1;
+                y_angle = 180f;
+                break;
+            case "SL2":
+                cur_path = path_CSV2;
+                y_angle = 60f;
+                break;
+            case "SL3":
+                cur_path = path_CSV3;
+                y_angle = -62f;
+                break;
+        }
+
+        using(var reader = new StreamReader(cur_path))
         {
             while (!reader.EndOfStream)
             {
@@ -20,19 +49,26 @@ public class MoveMotor : MonoBehaviour
 
                 timeList.Add(values[0]);
                 angleList.Add(values[1]);
-                Debug.Log("value 0");
-                Debug.Log(values[0].ToString());
-                Debug.Log("value 1");
-                Debug.Log(values[1].ToString());
+                // Debug.Log("value 0");
+                // Debug.Log(values[0].ToString());
+                // Debug.Log("value 1");
+                // Debug.Log(values[1].ToString());
             }
             reader.Close();
         }
-        Debug.Log(timeList.Count.ToString());
+        // Debug.Log(timeList.Count.ToString());
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (ang_cnt < angleList.Count)
+        {
+            transform.eulerAngles = new Vector3(0f, y_angle, float.Parse(angleList[ang_cnt]));
+            // Debug.Log(float.Parse(angleList[ang_cnt]).ToString());
+            // Debug.Log("count: " + ang_cnt.ToString());
+            ang_cnt++;
+        }
         
     }
 }
